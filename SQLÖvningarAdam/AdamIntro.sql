25bbf0157abc298c004f258a727c51e5994abeec
arybbe <123965769+arybbe@users.noreply.github.com>
2023-11-13 14:39:21 +0100

    Add files via upload

---
+++ SQLÖvningarAdam/AdamIntro.sql
@@ -0,0 +1,24 @@
+--CREATE DATABASE KlassenDb;
+USE KlassenDb
+
+--CREATE TABLE KlassenTbl(
+--	Id int PRIMARY KEY,
+--	Förnamn nvarchar(max) NOT NULL,
+--	Efternamn nvarchar(max) NOT NULL,
+--	Ålder int,
+--	Hemort nvarchar(max) NOT NULL
+--	);
+
+INSERT INTO KlassenTbl
+VALUES(1, 'Adam', 'Ryberg', 20, 'Lindome')
+
+INSERT INTO KlassenTbl
+VALUES(2, 'Tobias', 'Ehlme', 22, 'Kålltorp')
+
+INSERT INTO KlassenTbl
+VALUES(3, 'Olle', 'Heimbrand', 30, 'Kungsbacka')
+
+INSERT INTO KlassenTbl
+VALUES(4, 'Markus', 'Hultman', 28, 'Göteborg')
+
+
